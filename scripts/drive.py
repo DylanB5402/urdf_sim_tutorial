105e02f74625cd11d80e23d2f83decd1fc7f1b76
Dylan <d.barva5402@gmail.com>
2020-12-25 19:24:50 -0800

    add drive script

---
+++ scripts/drive.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+# linear: x is forward vel in m/s, angular: z is yaw in rad/s
+def drive(linear_vel, angular_vel):
+    velocity_publisher = rospy.Publisher('r2d2_diff_drive_controller/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('drive', anonymous=True)
+    velocity_msg = Twist()
+    velocity_msg.linear.x = linear_vel
+    velocity_msg.angular.z = angular_vel
+    velocity_publisher.publish(velocity_msg)
+
+if __name__ == '__main__':
+    try:
+        drive(0.4, 0)
+    except rospy.ROSInterruptException:
+        pass
