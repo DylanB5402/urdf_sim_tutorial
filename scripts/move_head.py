d4c397a7f05f932dd93612e0df876d3c98011827
Dylan <d.barva5402@gmail.com>
2020-12-28 20:15:12 -0800

    setting head position via python script works

---
+++ scripts/move_head.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+import math
+
+# type of setpoint is either angle (rad) or angular velocity (rad/s) as specified by the controller type in head.yaml
+def go_to_setpoint(setpoint):
+    setpoint_publisher = rospy.Publisher("/r2d2_head_controller/command", Float64, queue_size=10)
+    rospy.init_node('head_setpoint', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        rospy.loginfo(setpoint)
+        setpoint_publisher.publish(setpoint)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        go_to_setpoint(0.7)
+    except rospy.ROSInterruptException:
+        pass
