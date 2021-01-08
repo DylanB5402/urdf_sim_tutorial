bb6eda7254891f11140abfc948554cece42d5b82
Dylan <d.barva5402@gmail.com>
2021-01-07 19:00:18 -0800

    tank drive works, gonna have to do something else for mecanum though

---
+++ scripts/mecanum_drive.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import rospy
+from rospy import Publisher, Rate 
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64MultiArray
--- scripts/tank_drive.py
+++ scripts/tank_drive.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
-from rospy import Publisher, Rate, 
+import rospy
+from rospy import Publisher, Rate 
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
+
 
 def tank_drive(leftVel, rightVel):
-    left_vel_publisher = Publisher()
+    vel_publisher = Publisher("/r2d2_mecanum_drive_controller/command", Float64MultiArray, queue_size=10)
+    rospy.init_node("drive_velocity_setpoint", anonymous=True)
+    vel_array = Float64MultiArray()
+    # left front, left_back, right_front right_back
+    vel_array.data = [leftVel, -leftVel, -rightVel, rightVel]
+    rate = Rate(10)
+    while not rospy.is_shutdown():
+        rospy.loginfo(vel_array)
+        vel_publisher.publish(vel_array)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        tank_drive(6, 6)
+    except rospy.ROSInterruptException:
+        pass
+        
