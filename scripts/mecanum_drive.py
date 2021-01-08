ee1f126a2d698ed72d0cd63d0ac11fe1bf098414
Dylan <d.barva5402@gmail.com>
2021-01-07 19:08:52 -0800

    get started with mecanum, as best i can

--- scripts/mecanum_drive.py
+++ scripts/mecanum_drive.py
@@ -4,3 +4,14 @@ import rospy
 from rospy import Publisher, Rate 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64MultiArray
+
+class MecanumDrive():
+
+    def __init__(self):
+        
+
+if __name__ == '__main__':
+    try:
+        mecanum_drive()
+    except rospy.ROSInterruptException:
+        pass
