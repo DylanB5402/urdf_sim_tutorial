dce9e3e6b1909d4cff8581e499d4b80413dbd06f
Dylan <d.barva5402@gmail.com>
2021-01-30 17:10:13 -0800

    small changes, still unstable

--- scripts/tank_drive.py
+++ scripts/tank_drive.py
@@ -11,7 +11,7 @@ def tank_drive(leftFrontVel, leftBackVel, rightFrontVel, rightBackVel):
     rospy.init_node("drive_velocity_setpoint", anonymous=True)
     vel_array = Float64MultiArray()
     # left front, left_back, right_front right_back
-    vel_array.data = [leftFrontVel, leftBackVel, rightFrontVel, rightBackVel]
+    vel_array.data = [leftFrontVel, leftBackVel, -rightFrontVel, -rightBackVel]
     rate = Rate(10)
     while not rospy.is_shutdown():
         rospy.loginfo(vel_array)
@@ -20,7 +20,7 @@ def tank_drive(leftFrontVel, leftBackVel, rightFrontVel, rightBackVel):
 
 if __name__ == '__main__':
     try:
-        tank_drive(3, 3, -3, -3)
+        tank_drive(15, 15, 15, 15)
     except rospy.ROSInterruptException:
         pass
         
